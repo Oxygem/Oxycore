8a0430c4141d4279e81840b6ab51601528165f2f
Nick Barrett <nick@oxygem.com>
2013-06-04 12:19:36 +0100

    added keepmes to force directory creation in git

---
+++ docs/examples/object.lua
@@ -0,0 +1,32 @@
+local object = {
+    posts = {
+        functionName = true,
+        anotherPost = true
+    }
+}
+
+--prepare an actual object
+function object:prepare()
+end
+
+--GET view
+function object:get()
+end
+
+--GET edit
+function object:edit()
+end
+
+--GET add
+function object:add()
+end
+
+
+
+--POST function
+function object:functionName()
+end
+
+--POST function
+function object:anotherPost()
+end
