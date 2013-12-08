528757522a86c6a22d987a33b7614c93555f2e79
Nick Barrett <nick@oxygem.com>
2013-12-08 23:14:34 +0000

    Fix bug with name saving

--- app/post/profile.lua
+++ app/post/profile.lua
@@ -25,6 +25,7 @@ if request.post.update == 'address' then
 
 	--update
 	local status, err = user:setData({
+		real_name = request.post.real_name,
 		address = request.post.address,
 		country = request.post.country
 	})
@@ -41,14 +42,16 @@ else
 		return template:error( 'Please provide an email and a name' )
 	elseif not request.post.email:find( '^[^@]+@' ) then
 		return header:redirect( '/profile', 'error', 'Please use a valid email address' )
+	elseif request.post.phone and not tonumber( request.post.phone ) then
+		return header:redirect( '/profile', 'error', 'Please enter a valid phone number' )
 	else
 		--update
 		local status, err = user:setData({
-			real_name = request.post.real_name,
 			name = request.post.name,
 			email = request.post.email,
+			phone = tonumber( request.post.phone ),
 			password = request.post.password,
-			company = request.post.company
+			two_factor = request.post.two_factor == 'on' and 1 or 0
 		})
 		if not status then
 			return header:redirect( '/profile', 'error', err )
