218af3d1f5e41e9741f4951fb7345dcdc3c6494e
Nick Barrett <nick@oxygem.com>
2014-02-22 23:01:17 +0000

    Inlined tiny JS bit for better ux

--- app/inc/js/noscript.js
+++
@@ -1,8 +0,0 @@
-// File: app/inc/js/noscript.js
-// Desc: hack around the no html inside noscript bullshit
-
-'use strict';
-
-util.each( util.elements( '.noscript' ), function( key, element ) {
-    element.css({ 'display': 'none' });
-});
