de13ad884b2cf530fcf799c1e0fece9ef8c37eca
Nick Barrett <nick@oxygem.com>
2014-03-10 01:10:58 +0000

    Add logs to admin

--- modules/admin/admin.lua
+++ modules/admin/admin.lua
@@ -1,12 +1,11 @@
 local user = luawa.user
+local oxy = oxy
 
 local admin = {
-	user = oxy.object:new( 'user' )
+    user = oxy.object:new( 'user' )
 }
 
---[[
-    generate subnav (cached in session)
-]]
+-- Build subnav
 function admin:subnav()
     local nav = {}
 
@@ -23,19 +22,19 @@ function admin:subnav()
         table.insert( modules, { title = oxy.config[module].name, link = '' } )
     end
     settings.submenus['Modules'] = modules
-    table.insert( nav, settings )
+    --table.insert( nav, settings )
 
     --users
     if user:checkPermission( 'ViewUser' ) then
         local users = { title = 'Users', link = '/users', admin = true, submenus = {} }
-        if user:checkPermission( 'AddUser' ) then users.submenus = { { { title = 'Add User', link = '/users/add' } } } end
+        if user:checkPermission( 'AddUser' ) then users.submenus = {{{ title = 'Add User', link = '/users/add' }}} end
         table.insert( nav, users )
     end
 
     --groups
     if user:checkPermission( 'ViewUserGroup' ) then
         local groups = { title = 'Groups', link = '/groups', admin = true }
-        if user:checkPermission( 'AddUserGroup' ) then groups.submenus = { { { title = 'Add Group', link = '/groups/add' } } } end
+        if user:checkPermission( 'AddUserGroup' ) then groups.submenus = {{{ title = 'Add Group', link = '/groups/add' }}} end
         table.insert( nav, groups )
     end
 
@@ -45,10 +44,23 @@ function admin:subnav()
         table.insert( nav, permissions )
     end
 
+    --objects
+    local owner_objects = { title = 'Objects', link = '/objects', admin = true }
+    local objects = {}
+    for name, object in pairs( oxy.config.objects ) do
+        if user:checkPermission( 'OwnerAny' .. object.permission ) then
+            table.insert( objects, { title = object.name, link = '/objects/' .. name  })
+        end
+    end
+    if #objects > 0 then
+        owner_objects.submenus = { objects }
+        --table.insert( nav, owner_objects )
+    end
+
     --logging
     if user:checkPermission( 'ViewLog' ) then
         local logs = { title = 'Logs', link = '/logs', admin = true }
-        table.insert( nav, logs )
+        --table.insert( nav, logs )
     end
 
     return nav
