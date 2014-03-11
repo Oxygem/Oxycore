f44222a7d0b9cd1a915c261cef8fd143ef6c82f8
Nick Barrett <nick@oxygem.com>
2014-03-11 02:16:28 +0000

    Add username to logs

--- modules/admin/get/logs.lua
+++ modules/admin/get/logs.lua
@@ -10,7 +10,12 @@ if not user:checkPermission( 'ViewLog' ) then
     return template:error( 'You do not have permission to do that' )
 end
 
-local logs, err = database:select( 'log', '*', false, { limit = 500, order = 'time DESC' })
+local logs, err = database:select(
+    'log LEFT JOIN user ON log.user_id = user.id',
+    { 'log.*', 'user.name AS username' },
+    false,
+    { limit = 500, order = 'time DESC' }
+)
 if not logs then
     return template:error( err )
 end
