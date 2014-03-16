63798350359417e6459f5b18946678417728b03b
Nick Barrett <nick@oxygem.com>
2014-03-16 23:19:28 +0000

    remove broken search

--- app/inc/js/search.js
+++ app/inc/js/search.js
@@ -4,20 +4,20 @@
 'use strict';
 
 var search = {
-	element: util.element( '#search' ),
+    element: util.element( '#search' ),
 
     toggle: function() {
         if( this.element.style.getPropertyValue( 'display' ) == 'block' ) {
-        	this.element.css( { display: 'none' } );
+            this.element.css( { display: 'none' } );
         } else {
-        	this.element.css( { display: 'block' } );
+            this.element.css( { display: 'block' } );
         }
     }
 };
 
 
 //bind
-document.querySelector( '#header form.search' ).addEventListener( 'submit', function( ev ) {
-    ev.preventDefault();
-    search.toggle();
-});
+//util.element( '#header form.search' ).addEventListener( 'submit', function( ev ) {
+ //   ev.preventDefault();
+ //   search.toggle();
+//});
